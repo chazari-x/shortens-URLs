28b19e3ded8de3e2934bd6b4f53a03c21a79d145
chazari <chalimovrz@gmail.com>
2023-03-30 20:02:19 +0500

    increment 8

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -55,7 +55,7 @@ func (w gzipWriter) Write(b []byte) (int, error) {
 
 func gzipHandle(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
+		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
 			next.ServeHTTP(w, r)
 			return
 		}
@@ -75,7 +75,7 @@ func gzipHandle(next http.Handler) http.Handler {
 			}
 		}(gz)
 
-		w.Header().Set("Accept-Encoding", "gzip")
+		w.Header().Set("Content-Encoding", "gzip")
 		next.ServeHTTP(gzipWriter{ResponseWriter: w, Writer: gz}, r)
 	})
 }
