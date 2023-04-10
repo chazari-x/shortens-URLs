2986c87e001d2650fd13d44b936709dd90c5d841
chazari <chalimovrz@gmail.com>
2023-04-10 21:41:31 +0500

    increment 11

--- internal/app/storage/storage.go
+++ internal/app/storage/storage.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
 	_ "github.com/lib/pq"
@@ -38,6 +39,12 @@ type Event struct {
 	ID   string `json:"id"`
 	URL  string `json:"url"`
 	User string `json:"user"`
+}
+
+type shortURL struct {
+	ID     int
+	URL    string
+	UserID string
 }
 
 type URLs struct {
@@ -139,6 +146,14 @@ func (c *Config) startDataBase() (*sql.DB, error) {
 		return nil, err
 	}
 
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS shortURL (" +
+		"id SERIAL PRIMARY KEY NOT NULL, " +
+		"url varchar NOT NULL, " +
+		"userID varchar NOT NULL)")
+	if err != nil {
+		return nil, err
+	}
+
 	return db, nil
 }
 
@@ -189,20 +204,37 @@ func (c *Config) startFileStorage() error {
 
 func (c *Config) Add(url, user string) (string, error) {
 	var id string
+	var err error
 	s.ID++
 
-	if c.FileStoragePath == "" {
-		id = strconv.FormatInt(int64(s.ID), 36)
-		s.URLs[s.ID] = Event{
-			ID:   id,
-			URL:  url,
-			User: user,
-		}
-
-		return id, nil
+	if c.DataBaseDSN != "" {
+		id, err = c.dbAdd(url, user)
+	} else if c.FileStoragePath != "" {
+		id, err = c.fileAdd(url, user)
+	} else {
+		id, err = c.memoryAdd(url, user)
 	}
 
-	id = strconv.FormatInt(int64(s.ID), 36)
+	if err != nil {
+		return "", err
+	}
+
+	return id, nil
+}
+
+func (c *Config) memoryAdd(url, user string) (string, error) {
+	id := strconv.FormatInt(int64(s.ID), 36)
+	s.URLs[s.ID] = Event{
+		ID:   id,
+		URL:  url,
+		User: user,
+	}
+
+	return id, nil
+}
+
+func (c *Config) fileAdd(url, user string) (string, error) {
+	id := strconv.FormatInt(int64(s.ID), 36)
 
 	producer, err := newProducer(c.FileStoragePath)
 	if err != nil {
@@ -224,24 +256,52 @@ func (c *Config) Add(url, user string) (string, error) {
 	return id, nil
 }
 
+func (c *Config) dbAdd(addURL, user string) (string, error) {
+	var id int
+
+	err := c.DB.QueryRow("INSERT INTO shortURL (url, userID) VALUES ($1, $2) RETURNING id", addURL, user).Scan(&id)
+	if err != nil {
+		return "", err
+	}
+
+	sID := strconv.FormatInt(int64(id-1), 36)
+
+	return sID, nil
+}
+
 func (c *Config) Get(str string) (string, error) {
 	id, err := strconv.ParseInt(str, 36, 64)
 	if err != nil {
 		return "", err
 	}
 
-	if c.FileStoragePath == "" {
+	if c.DataBaseDSN == "" {
 		if int(id) > s.ID {
 			return "", fmt.Errorf("the storage is empty or the element is missing")
 		}
-
-		return s.URLs[int(id)].URL, nil
 	}
 
-	if int(id) > s.ID {
-		return "", fmt.Errorf("the storage is empty or the element is missing")
+	var url string
+	if c.DataBaseDSN != "" {
+		url, err = c.dbGet(int(id) + 1)
+	} else if c.FileStoragePath != "" {
+		url, err = c.fileGet(str)
+	} else {
+		url, err = c.memoryGet(int(id))
 	}
 
+	if err != nil {
+		return "", err
+	}
+
+	return url, nil
+}
+
+func (c *Config) memoryGet(id int) (string, error) {
+	return s.URLs[id].URL, nil
+}
+
+func (c *Config) fileGet(str string) (string, error) {
 	consumer, err := newConsumer(c.FileStoragePath)
 	if err != nil {
 		return "", err
@@ -266,21 +326,55 @@ func (c *Config) Get(str string) (string, error) {
 	return "", fmt.Errorf("the storage is empty or the element is missing")
 }
 
-func (c *Config) GetAll(user string) ([]URLs, error) {
-	var UserURLs []URLs
-	if c.FileStoragePath == "" {
-		for _, i := range s.URLs {
-			if i.User == user {
-				UserURLs = append(UserURLs, URLs{
-					ShortURL:    "http://" + c.ServerAddress + c.BaseURL + i.ID,
-					OriginalURL: i.URL,
-				})
-			}
-		}
+func (c *Config) dbGet(id int) (string, error) {
+	var dbItem shortURL
 
-		return UserURLs, nil
+	err := c.DB.QueryRow("SELECT * FROM shortURL WHERE id = $1", id).Scan(&dbItem.ID, &dbItem.URL, &dbItem.UserID)
+	if err != nil {
+		if strings.Contains(err.Error(), "no rows in result set") {
+			return "", fmt.Errorf("the storage is empty or the element is missing")
+		}
+		return "", err
 	}
 
+	return dbItem.URL, nil
+}
+
+func (c *Config) GetAll(user string) ([]URLs, error) {
+	var userURLs []URLs
+	var err error
+
+	if c.DataBaseDSN != "" {
+		userURLs, err = c.dbGetAll(user)
+	} else if c.FileStoragePath != "" {
+		userURLs, err = c.fileGetAll(user)
+	} else {
+		userURLs, err = c.memoryGetAll(user)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return userURLs, nil
+}
+
+func (c *Config) memoryGetAll(user string) ([]URLs, error) {
+	var UserURLs []URLs
+	for _, i := range s.URLs {
+		if i.User == user {
+			UserURLs = append(UserURLs, URLs{
+				ShortURL:    "http://" + c.ServerAddress + c.BaseURL + i.ID,
+				OriginalURL: i.URL,
+			})
+		}
+	}
+
+	return UserURLs, nil
+}
+
+func (c *Config) fileGetAll(user string) ([]URLs, error) {
+	var UserURLs []URLs
 	consumer, err := newConsumer(c.FileStoragePath)
 	if err != nil {
 		return UserURLs, err
@@ -303,6 +397,33 @@ func (c *Config) GetAll(user string) ([]URLs, error) {
 				OriginalURL: readEvent.URL,
 			})
 		}
+	}
+
+	return UserURLs, nil
+}
+
+func (c *Config) dbGetAll(user string) ([]URLs, error) {
+	var UserURLs []URLs
+
+	rows, err := c.DB.Query("SELECT * FROM shortURL WHERE userID = $1", user)
+	if err != nil {
+
+		return nil, err
+	}
+
+	for rows.Next() {
+		var dbItem shortURL
+		err = rows.Scan(&dbItem.ID, &dbItem.URL, &dbItem.UserID)
+		if err != nil {
+			if strings.Contains(err.Error(), "no rows in result set") {
+				return nil, fmt.Errorf("the storage is empty or the element is missing")
+			}
+			return nil, err
+		}
+		UserURLs = append(UserURLs, URLs{
+			ShortURL:    "http://" + c.ServerAddress + c.BaseURL + strconv.Itoa(dbItem.ID),
+			OriginalURL: dbItem.URL,
+		})
 	}
 
 	return UserURLs, nil
